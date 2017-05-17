c59c14aab3079d0f0f9303fdfe26818fd2770ae7
53983 <539833637@qq.com>
2017-05-17 23:03:06 +0800

    覆盖更新

--- js/controllers/order_orderList.js
+++ js/controllers/order_orderList.js
@@ -1,7 +1,7 @@
 /**
- * Created by Kingson·liu on 17/3/8.
+ * Created by kingson·liu on 17/3/8.
  */
-goceanApp.controller('OrderListCtrl', function ($scope, $rootScope, $state, $timeout, $stateParams,orderListService,configService) {
+goceanApp.controller('OrderQiusongCtrl', function ($rootScope,$scope, $state, $timeout, $stateParams, qiusongListService, configService) {
 
     var params = configService.parseQueryString(window.location.href);
     if (params.passportId){
@@ -10,7 +10,7 @@ goceanApp.controller('OrderListCtrl', function ($scope, $rootScope, $state, $tim
     }
 
     if ($rootScope.passport == null ){
-        var _state = "order.orderList";
+        var _state = "order.qiusong";
         window.location = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx0cae6e3b9632e632&redirect_uri=http://wxsdk.yezaigou.com/wx/page/base&response_type=code&scope=snsapi_base&state="+_state;
         return;
     }
@@ -21,15 +21,15 @@ goceanApp.controller('OrderListCtrl', function ($scope, $rootScope, $state, $tim
     },100);
 
     // 底部tab选中
-    $("#order").addClass("weui_active").siblings().removeClass('weui_active');
+    $("#qiusong").addClass("weui_active").siblings().removeClass('weui_active');
     $('#tab1').tab({defaultIndex:0,activeClass:"tab-green"});
 
     $scope.page = 1;
     $scope.rows = 10;
-    var status = 'ALL';
+    var status = 'ING';
     //请求参数
     // 全部数据
-    $scope.listOrder = function(status){
+    $scope.listQiusong = function(status){
         var obj = {
             page:$scope.page,
             rows:$scope.rows,
@@ -37,86 +37,75 @@ goceanApp.controller('OrderListCtrl', function ($scope, $rootScope, $state, $tim
             token:$rootScope.passport.token,
             status:status};
 
-            orderListService.listOrder(obj).then(function(data){
-                console.log(data);
-                if ("OK" == data.status){
-                    var orderList = data.result;
-                    initView(orderList);
-                    $scope.orderList = orderList;
-                }
-            },function(err){
+        qiusongListService.listQiusong(obj).then(function(data){
+            console.log(data);
+            if ("OK" == data.status){
+                var qiusongList = data.result;
+                initView(qiusongList);
+                $scope.qiusongList = qiusongList;
+            }
+        },function(err){
 
-            });
+        });
     };
     // 页面刷新加载
-    $scope.listOrder(status);
+    $scope.listQiusong(status);
 
-    function initView(orderList){
-        for (i in orderList){
-            var brief = orderList[i];
-            if (brief.status == "ORDER_CREATED"){
-                brief.statusView = "未付款";
-            }else if (brief.status == "ORDER_FINISHED"){
+    function initView(qiusongList){
+        for (i in qiusongList){
+            var brief = qiusongList[i];
+            if (brief.crowdFunding.status == "ING"){
+                brief.statusView = "求送中";
+            }else if (brief.crowdFunding.status == "SUCCESSED"){
                 brief.statusView = "已完成";
-            }else if (brief.type == "FORWARD_PLAN" && brief.status == "ORDER_PAID"){
-                brief.statusView = "服务中";
-            }else if (brief.type == "NORMAL"){
-                if (brief.deliveryStatus == null){
-                    brief.statusView = "待发货";
-                }else if(brief.deliveryStatus == "DELIVERING"){
-                    brief.statusView = "送货中";
-                }
+            }else if (brief.crowdFunding.status == "SETTLED"){
+                brief.statusView = "已结算";
             }
+
             var itemList = brief.itemList;
             for (j in itemList){
                 var item = itemList[0];
                 item.priceView = "¥" + item.price;
-                if (item.pricePlus > 0){
-                    item.priceView += " + "+item.pricePlus;
-                }
             }
         }
     }
 
     // 跳转详情页
-    $scope.goToOrderDetail = function (order) {
-        $rootScope.orderDetailsView = null;
+    $scope.goToQiusongDetail = function (qiusong) {
+        $rootScope.qiusongDetailsView = null;
 
-        if (order.status == "ORDER_CREATED"){
-            $state.go('orderDetail', {orderId : order.id});
+        if (qiusong.payStatus == "UN_PAY" && qiusong.sponsorId != $rootScope.passport.passportId){
+            $state.go('qiusongPrepay', {qiusongId: qiusong.crowdFundingId.id, memberId: $rootScope.passport.passportId});
         }else {
 
-            var obj = {
-                passportId: $rootScope.passport.passportId,
-                token: $rootScope.passport.token,
-                orderId: order.id
-            };
-            orderListService.getDetails(obj).then(function (data) {
-                if ("OK" == data.status) {
-                    var orderDetailsDto = data.result;
-                    $rootScope.orderDetailsView = orderDetailsDto;
-                    $rootScope.orderDetailsView.isInited = false;
-
-                    adapteDetails(order);
-                }
-            }, function (err) {
-
-            });
+            adaptDetails(qiusong);
+            // var obj = {
+            //     passportId: $rootScope.passport.passportId,
+            //     token: $rootScope.passport.token,
+            //     crowdFundingId: qiusong.crowdFunding.id
+            // };
+            // qiusongListService.getDetails(obj).then(function (data) {
+            //     if ("OK" == data.status) {
+            //         var qiusongDetailsDto = data.result;
+            //         $rootScope.qiusongDetailsView = qiusongDetailsDto;
+            //         $rootScope.qiusongDetailsView.isInited = false;
+            //
+            //         adaptDetails(qiusong);
+            //     }
+            // }, function (err) {
+            //
+            // });
         }
 
-    }
+    };
 
-    function adapteDetails(order){
-        var id = order.id;
-        var status = order.status;
-        if (status == 'ORDER_PAID' || status == 'ORDER_FINISHED'){
-            if (order.type == 'FORWARD_PLAN'){
-                $state.go('inServiceDetail', {orderId : id});
-            } else if (order.type == 'NORMAL') {
-                $state.go('normalDetail', {orderId: id});
-            }
-        } else if (status == "ORDER_CREATED"){
-            $state.go('orderDetail', {orderId : id});
+    function adaptDetails(qiusong){
+        var id = qiusong.crowdFunding.id;
+        var status = qiusong.status;
+        if (qiusong.crowdFunding.sponsorId == $rootScope.passport.passportId){
+            $state.go("qiusongDetailsSponsor",{id:id});
+        } else{
+
         }
     }
 });
