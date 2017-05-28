120dc01b47b3dc2624c9f29e2fc658dd587d07ac
53983 <539833637@qq.com>
2017-05-28 11:33:53 +0800

    账户明细

--- js/controllers/mineDetail.js
+++ js/controllers/mineDetail.js
@@ -4,4 +4,8 @@
 goceanApp.controller('MineDetailCtrl', function ($scope, $rootScope, $state, $timeout, $stateParams, dealDetailService, configService) {
     console.log('about MineDetailCtrl');
 
+    // 获取JSSDK
+    configService.getJssdkInfo(window.location.href);
+    // 隐藏右上角
+    configService.hideWXBtn();
 });
